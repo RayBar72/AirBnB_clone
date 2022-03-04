ca430e818f2e1970d60388aea12ab32c7b5d0bed
Matias <3959@holbertonschool.com>
2022-03-04 06:13:06 -0800

    actualizwe

--- console.py
+++ console.py
@@ -2,6 +2,9 @@
 """entry point of the command interpreter
 """
 
+import argparse
+from models import storage
+from models.user import User
 import cmd
 from models.base_model import BaseModel
 
@@ -9,36 +12,109 @@ class HBNBCommand(cmd.Cmd):
     """Creation of class"""
     prompt = '(hbnb)'
 
-    def emptyline(self):
-        """EmptyLine"""
-        pass
-    
-    def do_quit(self, line):
-        """Quit command to EXIT the program"""
-        return True
-
     def do_EOF(self, line):
-        """Exit using ctrl + C"""
+        """Using EOF"""
         return True
 
-    """-------CONSOLE 0.01-------------"""
-    def do_create(self, args):
-        """Method create new instance"""
-        if not args:
+    def do_quiet(self, line):
+        """Quiet for exit the command interpreter"""
+        return True
+
+    def empyline(self):
+        """Passing when empty line"""
+        pass
+
+    def do_create(self, line):
+        """Creates an instance"""
+        if line == "" or line is None:
             print("** class name missing **")
-        elif args not:
-            print("** class doesn´t exist **")
+        elif line not in storage.classes():
+            print("** class doesn't exist **")
         else:
-            new_class = BaseModel()
-            new_class.save()
-            print(new_class.id)
-    
-    def do_show(self, args):
-        """ Method show the representation
-            of the instance
+            x = BaseModel()
+            x.save()
+            print(x.id)
+
+    def do_show(self, line):
         """
+        String representation of an instance
+        based on the name and id. It is printed
+        """
+        i
+        if line == "" or line is None:
+            print("** class name missing **")
+        else:
+            tokens = line.:x
+            split(" ")
+            if tokens[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(tokens) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(tokens[0], tokens[1])
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    print(storage.all()[key])
+
+    def do_destroy(self, line):
+        """
+        Deletes an instance based on the class
+        name and id (save the change into the JSON file).
+        """
+        if line == "" or line is None:
+            print("** class name missing **")
+        else:
+            tokens = line.split(" ")
+            if tokens[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(tokens) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(tokens[0], tokens[1])
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    del storage.all()[key]
+                    storage.save()
+
+    def do_all(self, line):
+        """
+        Prints all string representation of all instances
+        based or not on the class name.
+        """
+        if line != "":
+            tokens = line.split(" ")
+            if tokens[0] not in storage.classes():
+                print("** class doesn't exist **")
+        for k in storage.all():
+            print(storage.all()[k])
+
+    def do_update(self, line):
+        """
+        Updates an instance based on the class name and id
+        by adding or updating attribute (save the change into
+        the JSON file).
+        """
+        if line == "" or line is None:
+            print("** class name missing **")
+        else:
+            tokens = line.split(" ")
+            if tokens[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(tokens) < 2:
+                print("** instance id missing **")
+            elif len(tokens) < 3:
+                print("** attribute name missing **")
+            elif len(tokens) < 4:
+                print("** value missing **")
+            else:
+                key = "{}.{}".format(tokens[0], tokens[1])
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    setattr(storage.all()[key], tokens[2], tokens[3])
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- models/engine/file_store.py
+++ models/engine/file_store.py
@@ -17,15 +17,35 @@ class FileStorage:
     __objects = "ni idea"
 
     def all(self):
-        """return dictionary objects"""
+        """Returns the dictionary of __objects"""
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """Sets __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        tmp_dictionary = {}
+        for k, v in self.__objects.items():
+            tmp_dictionary[k] = v.to_dict()
+        with open(self.__file_path, "w", encoding="utf-8") as working_file:
+            json.dump(tmp_dictionary, working_file)
+
+    def classes(self):
+        """Dictionary with valid classes"""
+        classes = {"BaseModel": BaseModel, "Place": Place,
+                    "City": City, "Amenity": Amenity, "Review": Review,
+                    "User": User}
+        return classes
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
-
+        """Deserializes the JSON file to __objects"""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as x:
+            jfiledict = json.load(x)
+            jfiledict = {k: self.classes()[v["__class__"]](
+                **v) for k, v in jfiledict.items()}
+            FileStorage.__objects = jfiledict
