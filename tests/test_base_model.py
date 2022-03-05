86ce5fa9f01125b5d98805dbc668295ab130e410
Matias <3959@holbertonschool.com>
2022-03-05 10:45:28 -0800

    add more test

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -6,6 +6,7 @@ import unittest
 from models.base_model import BaseModel
 import models
 from datetime import datetime
+import uuid
 
 
 class Test_clase_m(unittest.TestCase):
@@ -24,6 +25,10 @@ class Test_clase_m(unittest.TestCase):
     def test_none(self):
         """ Test for base if thi os none """
         self.assertIsNone(None, self.Model.__dict__.values())
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.__str__.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
 
     def test_ID(self):
         """ Test for ID """
@@ -31,6 +36,9 @@ class Test_clase_m(unittest.TestCase):
         id = model_id.id
         self.assertNotEqual(id, self.Model.id)
         self.assertEqual(type(id), str)
+        self.assertTrue(hasattr(model_id, "id"))
+        uniqid = uuid.UUID(id)
+        self.assertEqual(uniqid.version, 4)
 
     def test_update(self):
         """ Test fot method save """
@@ -48,6 +56,7 @@ class Test_clase_m(unittest.TestCase):
         self.Model.my_number = 2022
         self.assertEqual(self.Model.name, "Holberton")
         self.assertEqual(self.Model.my_number, 2022)
+        self.assertEqual(type(self.Model.name), str)
 
     def test_class(self):
         """ test for class method """
@@ -60,7 +69,21 @@ class Test_clase_m(unittest.TestCase):
 
     def test_dict(self):
         """ Test for dict"""
+        json = self.Model.to_dict()
         self.assertEqual(type(self.Model.__dict__), dict)
+        self.assertEqual(json['__class__'], 'BaseModel')
+
+    def test_creation_dic(self):
+        """Test for correct creation dictionary"""
+        date = datetime.now()
+        dic = {"id": "7734cf23-6c89-4662-8483-284727324c77", "created_at":
+               "2022-03-4T16:32:39.023915", "updated_at":
+               "2022-03-4T16:32:39.023940", "__class__": "BaseModel"}
+        my_base = BaseModel(**dic)
+        self.assertEqual(my_base.__class__.__name__, "BaseModel")
+        self.assertEqual(my_base.id, "7734cf23-6c89-4662-8483-284727324c77")
+        self.assertEqual(type(my_base.created_at), type(date))
+        self.assertEqual(type(my_base.updated_at), type(date))
 
     def test_str(self):
         """test for str method"""
@@ -68,6 +91,11 @@ class Test_clase_m(unittest.TestCase):
         model_str = f'[{BaseModel.__name__}] ({Model.id}) {Model.__dict__}'
         self.assertEqual(model_str, str(Model))
     
+    def test_save(self):
+        """Test for save method"""
+        self.Model.save()
+        self.assertNotEqual(self.Model.created_at, self.Model.updated_at)
+
     def setUp(self):
         """Setting casses for testing enviroment"""
         self.b1 = BaseModel()
