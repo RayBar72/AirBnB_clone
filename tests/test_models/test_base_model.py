8293840f4a7e6b847a7f9fdf04102984f59df52c
Matias <3959@holbertonschool.com>
2022-03-05 11:40:13 -0800

    add test for base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -67,6 +67,7 @@ class Test_clase_m(unittest.TestCase):
         """ Test for data"""
         self.assertEqual(isinstance(self.Model.created_at, datetime), True)
         self.assertEqual(isinstance(self.Model.updated_at, datetime), True)
+        self.assertEqual(type(self.Model.created_at), datetime)
 
     def test_dict(self):
         """ Test for dict"""
