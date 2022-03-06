bfc2a493f4d3b5f25618715b27e4390ad0997848
Matias <3959@holbertonschool.com>
2022-03-06 15:24:08 -0800

    add test for review

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,12 @@
+#!/user/bin/python3
+"""test for review file"""
+
+from models.review import Review
+import unittest
+
+
+class Test_review(unittest.TestCase):
+    """test for correct funcioning of review"""
+     def test_doc(self):
+         """test for documentation"""
+         self.assertTrue(len(Review.__doc__) > 0)
