1c4f0f249678b7d56acf7a4dea27cffd7ed5a4e3
Matias <3959@holbertonschool.com>
2022-03-06 10:27:53 -0800

    add more test for state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -11,12 +11,18 @@ class Test_state(unittest.TestCase):
     """
     Test for the correct functioning of state
     """
-    def test_valdate(self):
+    def test_validate(self):
         """ validate arguments save"""
         st = State()
         st.name = "Montevideo"
         self.assertTrue(hasattr(st, "name"))
         self.assertIsInstance(st.name, str)
+    
+    def test_ids(self):
+        """ test for two ids"""
+        self._state1 = State()
+        self._state2 = State()
+        self.assertNotEqual(self._state1.id, self._state2.id)
 
     if __name__ == '__main__':
         unittest.main()
